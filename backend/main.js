dfdf516dbf527f5270fbbc4cb84b7e799f5219df
momo391 <ia7636hs@gmail.com>
2024-12-07 20:43:29 +0100

    backend completed

---
+++ backend/main.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const accountRouter = require("./routes/api/account.js");
+
+const port = process.env.PORT;
+const uri = process.env.URI;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose
+  .connect(uri)
+  .then(() => console.log("connected to database"))
+  .catch((err) => console.log("err", err));
+
+app.use("/api/account", accountRouter);
+
+app.listen(port, () => {
+  console.log(`listening on http://localhost:${port}`);
+});
